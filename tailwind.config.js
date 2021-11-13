f1df4aeeb3a0dee363dfd0d43a55cb0c14f0d7c3
AriSmrd7 <arismrd13@gmail.com>
2021-11-13 12:22:06 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    theme: {
+      container: {
+        center: true,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
